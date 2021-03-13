34e49f4de4efa5fdb7157baa967ca50c88761238
cliff000 <cliff.5963@gmail.com>
2021-03-13 14:39:31 +0900

    変更取り消し

--- TeamDev2021/main.cpp
+++ TeamDev2021/main.cpp
@@ -1,27 +1,6 @@
 #include "DxLib.h"
 
 
-int mouse;
-int mouseKeep;
-int *mouseX, *mouseY;
-
-
-void update() {
-	mouse = GetMouseInput(); //マウスの入力状態取得
-
-	//マウスが左クリックされていれば位置を取得
-	if (mouse & MOUSE_INPUT_LEFT) {
-		GetMousePoint(mouseX, mouseY);
-	}
-}
-
-void draw() {
-	//マウスが左クリックされていれば描画
-	if (mouse & MOUSE_INPUT_LEFT) {
-		GetMousePoint(mouseX, mouseY);
-	}
-}
-
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
 
